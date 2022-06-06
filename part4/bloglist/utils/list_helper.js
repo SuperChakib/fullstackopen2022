d887f3fcda07511fa22ca9a1e3425b8e3ee3fd08
Chakib <mijikaihaikingu@tutanota.com>
2022-06-06 15:45:20 +0200

    Part4 helper step3 define & test favoriteBlog function

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,7 +2,20 @@ const dummy = blogs => 1
 
 const totalLikes = blogs => blogs.reduce((sum, item) => sum + item.likes, 0)
 
+const favoriteBlog = blogs => {
+  const reducer = (best, next) => {
+    const { title, author, likes } = next
+
+    return (best.likes || 0) > next.likes
+      ? best
+      : { title, author, likes }
+  }
+
+  return blogs.reduce(reducer, {})
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
