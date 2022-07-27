bc6e50bb8fff1251dc061c89ef72990ff7c173f3
Chakib <mijikaihaikingu@tutanota.com>
2022-07-27 09:53:53 +0200

    Duplicate previous version to have separate folders for anecdotes and anecdotes-backend

---
+++ part6/anecdotes/src/App.js
@@ -0,0 +1,21 @@
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
+import Notification from './components/Notification'
+import Filter from './components/Filter'
+import { useSelector } from 'react-redux'
+
+const App = () => {
+  const filter = useSelector(state => state.notification)
+  
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <Filter />
+      {filter ? <Notification /> : null}
+      <AnecdoteList />
+      <AnecdoteForm />
+    </div>
+  )
+}
+
+export default App
---
+++ part6/anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,29 @@
+import { useDispatch } from "react-redux"
+import { createAnecdote } from "../reducers/anecdoteReducer"
+import { removeNotification, setNotification } from "../reducers/notificationReducer"
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const content = e.target.anecdote.value
+    e.target.anecdote.value = ''
+
+    dispatch(createAnecdote(content))
+    dispatch(setNotification(`you created '${content}'`))
+    setTimeout(() => dispatch(removeNotification()), 5000)
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={handleSubmit}>
+        <div><input name='anecdote' /></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
