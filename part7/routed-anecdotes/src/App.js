6eb9a176f300f154cd0f05a4d9bdc19b01743903
Chakib <mijikaihaikingu@tutanota.com>
2022-07-30 19:45:42 +0200

    Part7 routed-anecdotes step3 implement navigate & notification after creation

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Link, Routes, Route, useMatch } from 'react-router-dom'
+import { Link, Routes, Route, useMatch, useNavigate } from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -100,6 +100,7 @@ const CreateNew = (props) => {
 }
 
 const App = () => {
+  const navigate = useNavigate()
   const [anecdotes, setAnecdotes] = useState([
     {
       content: 'If it hurts, do it more often',
@@ -122,6 +123,11 @@ const App = () => {
   const addNew = (anecdote) => {
     anecdote.id = Math.round(Math.random() * 10000)
     setAnecdotes(anecdotes.concat(anecdote))
+    
+    navigate('/')
+
+    setNotification(`a new anecdote '${anecdote.content}' created!`)
+    setTimeout(() => setNotification(''), 5000)
   }
 
   const anecdoteById = (id) =>
@@ -147,6 +153,7 @@ const App = () => {
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
+      {notification ? <p>{notification}</p> : null}
       <Routes>
         <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />}></Route>
         <Route path='/anecdotes/:id' element={<Anecdote anecdote={anecdote} vote={vote} />}></Route>
